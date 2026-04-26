5fc0e36826fdc9e156b890e13cb0982255997acf
Jesus Mendez <jesus@email.com>
2026-04-26 10:13:56 +0200

    modified:   backend/app/Http/Middleware/Cors.php 	modified:   frontend/angular.json

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -12,12 +12,13 @@ class Cors
             return response('', 200)
                 ->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept')
+                ->header('Access-Control-Max-Age', '86400');
         }
 
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
     }
 }
